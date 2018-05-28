cbe7c6b185db79a63e22a045dc340f13b9198742
Pavel Naumov <pavliknaumov@gmail.com>
2018-05-28 22:13:05 +0100

    I have added edit and create methods. Also, have created the corresponding view pages: edit page that renders the form

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,8 +1,14 @@
 class JokesController < ApplicationController
+
+  before_action :set_joke, only: [:show, :edit, :update, :destroy]
+
+
   def index
+    
   end
 
   def show
+    
   end
 
   def new
@@ -12,11 +18,31 @@ class JokesController < ApplicationController
   end
 
   def edit
+    @joke = Joke.find(params[:id])
   end
 
   def update
+     
+    respond_to do |format|
+      if @joke.update(joke_params)
+        redirect_to @joke, notice: 'Joke was successfully updated.'
+      else
+        render :new
+      end
+    end
   end
 
   def destroy
+
+  end
+
+  private
+
+  def set_joke
+    @joke = Joke.find(params[:id])
+  end
+
+  def params_joke
+    params.require(:joke).permit(:title, :description, :rating)
   end
 end
