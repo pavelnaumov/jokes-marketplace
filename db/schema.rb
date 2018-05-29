b85d99046cba9eebe226cca3750b36ef66dc32df
Pavel Naumov <pavliknaumov@gmail.com>
2018-05-29 11:31:11 +0100

    solved all the problems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_144144) do
+ActiveRecord::Schema.define(version: 2018_05_29_102652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_05_28_144144) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_jokes_on_user_id"
   end
 
