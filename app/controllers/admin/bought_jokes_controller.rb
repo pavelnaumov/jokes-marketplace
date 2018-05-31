2e52414e754d2855e4727662bb9d5b8c60f9f778
Laura Lotti <lauralotti@mac.com>
2018-05-31 16:09:06 +0100

    added index and create routes and actions for index and create bought jokes

--- app/controllers/admin/bought_jokes_controller.rb
+++ app/controllers/admin/bought_jokes_controller.rb
@@ -1,22 +1,31 @@
 class Admin::BoughtJokesController < ApplicationController
   before_action :set_bought_joke, only: [:show, :destroy]
+  before_action :set_joke, only: :create
 
   def index
-
+    @bought_jokes = BoughtJoke.all
   end
 
   def show
-    
+
   end
 
   def destroy
 
   end
 
-  def create
-    
+  def new
+    @bought_joke = BoughtJoke.new
   end
 
+  def create
+    @bought_joke = BoughtJoke.new
+    @bought_joke.user = current_user
+    @bought_joke.joke = Joke.find(params[:joke_id])
+    @bought_joke.save
+
+    redirect_to admin_joke_bought_jokes_path(:id)
+  end
 
   private
 
