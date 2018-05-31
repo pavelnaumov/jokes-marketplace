1a563a13d43cd3a616b38a28af32764ceffd31e9
Pavel Naumov <pavliknaumov@gmail.com>
2018-05-31 11:37:20 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   root to: "jokes#index"
 
-  resources :jokes, except: :index
+  resources :jokes, except: :index do 
+    resources :bought_jokes, only: [:index, :show, :destroy]
 
   namespace :admin do
     resources :jokes, only: [:index, :edit, :update, :destroy]
   end
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
