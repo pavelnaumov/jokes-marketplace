595f21b0a5aa4a977b61eff7f2ef340ead682d1f
Pavel Naumov <pavliknaumov@gmail.com>
2018-05-31 11:40:46 +0100

    created a controller

---
+++ app/controllers/bought_jokes_controller.rb
@@ -0,0 +1,7 @@
+class BoughtJokesController < ApplicationController
+  def index
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bought_jokes/index'
+  get 'bought_jokes/destroy'
   devise_for :users
 
   root to: "jokes#index"
