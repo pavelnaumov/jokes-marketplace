7e0054cbbfa4d8ec1fbcafe3451fd9beb7027249
Maria Castro <mariacantocastro@gmail.com>
2018-06-01 16:02:10 +0100

    favorite

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,3 +22,10 @@ clickOnButtonArrowToEditAndDelete();
 
 import { mouseoverEditAndDelete } from '../components/adminCard';
 mouseoverEditAndDelete();
+
+
+// import { rotateCards } from '../components/rotateCard';
+// rotateCards();
+
+import { favorite } from '../components/favorite';
+favorite();
