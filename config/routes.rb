0ce56398c8aedbd1d442c0df082701f32ddfad32
mariacastro96 <34350838+mariacastro96@users.noreply.github.com>
2018-06-01 16:20:15 +0100

    Revert "favorite"

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+  get 'bought_jokes/index'
+  get 'bought_jokes/destroy'
   devise_for :users
+
   root to: "jokes#index"
   namespace :admin do
-    patch 'bought_jokes/:id/favorite', to: "bought_jokes#favorite", as: :favorite
-    patch 'bought_jokes/:id/unfavorite', to: "bought_jokes#unfavorite", as: :unfavorite
-    resources :bought_jokes, only: [:index, :show, :destroy]
     resources :jokes, only: [:index, :destroy, :show, :edit, :update] do
-      resources :bought_jokes, only: [:create]
+      resources :bought_jokes, only: [:index, :show, :destroy, :create]
     end
   end
-  resources :jokes, only: [:index, :new, :create]
-end
 
+    resources :jokes, only: [:index, :new, :create]
+  end
